05b020ac542d474a5d79f69cc58b65eb38c8b88c
Laura Binish <laura.binish@gmail.com>
2019-12-02 21:41:43 +0200

    pamoka8

---
+++ JS/JsUzduotys.js
@@ -0,0 +1,18 @@
+// 2
+let vardas = "Ieva";
+let pavarde = "Gaubaite";
+let kursas = 4;
+let mokosi =  "VDU";
+let pazymiai = [10, 8, 9, 10];
+
+// 3
+
+document.getElementById('3').innerHTML =vardas+" "+pavarde+" "+" mokosi "+mokosi+" "+kursas+" kurse. Jos pazymiai: ["+pazymiai+"} ";
+
+// 4
+
+document.getElementById('vardas').innerHTML ="Vardas yra "+typeof(vardas);
+document.getElementById('pavarde').innerHTML ="Pavarde yra "+typeof(pavarde);
+document.getElementById('kursas').innerHTML ="Kursas yra "+typeof(kursas);
+document.getElementById('mokosi').innerHTML ="Mokosi yra "+typeof(mokosi);
+document.getElementById('pazymiai').innerHTML ="Pazymiai yra "+typeof(pazymiai);
---
+++ JS/loader.js
@@ -0,0 +1,25 @@
+data = '[{"blue" : "is ok", "red" : "is my fave color"}]';
+
+function load(text) {
+    var menu = JSON.parse(text);
+    let menuString = '';
+    for(let i=0; i<menu.length; i++){
+        let menuItem=menu[i];
+        menuString+= '<li><a href="'+ menuItem.adresas+'">'+menuItem.pavadinimas+'</a></li>';
+    }
+    document.querySelector('header> ul').innerHTML = menuString;
+}
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+loadJSON(load);
