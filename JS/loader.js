d14226c2280206fc878050588af49ac4f744ce55
Laura Binish <laura.binish@gmail.com>
2019-12-03 21:32:36 +0200

    PAskaita9

--- JS/loader.js
+++ JS/loader.js
@@ -3,12 +3,13 @@ data = '[{"blue" : "is ok", "red" : "is my fave color"}]';
 function load(text) {
     var menu = JSON.parse(text);
     let menuString = '';
-    for(let i=0; i<menu.length; i++){
-        let menuItem=menu[i];
-        menuString+= '<li><a href="'+ menuItem.adresas+'">'+menuItem.pavadinimas+'</a></li>';
+    for (let i = 0; i < menu.length; i++) {
+        let menuItem = menu[i];
+        menuString += '<li><a href="' + menuItem.adresas + '">' + menuItem.pavadinimas + '</a></li>';
     }
     document.querySelector('header> ul').innerHTML = menuString;
 }
+
 function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
@@ -22,4 +23,16 @@ function loadJSON(callback) {
     };
     xobj.send(null);
 }
+
 loadJSON(load);
+
+$(function () {
+    $('header ul li a').click(function (e){
+        e.preventDefault();
+
+        let elementas = $(this);
+        let adresas = elementas.attr('href');
+        $('main').load(adresas);
+    });
+
+});
