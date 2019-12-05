5b1e168c617463295c4df00a78cab27228842460
Laura Binish <laura.binish@gmail.com>
2019-12-05 02:47:43 +0200

    mid-night touch-ups

--- JS/final.js
+++ JS/final.js
@@ -10,12 +10,20 @@ function invisible() {
 
 function highlight() {
     this.classList.add('hlight');
+    let allHCards = document.getElementsByClassName('hlight');
+    if (allHCards.length === 1) {
+        second = 0;
+        minute = 0;
+
+        startTimer();
+    }
 
 
     if (!hasSelectedCard) {
         hasSelectedCard = true;
         firstCard = this;
         console.log({hasSelectedCard, firstCard})
+
     } else {
         hasSelectedCard = false;
         secondCard = this;
@@ -33,19 +41,44 @@ function highlight() {
         }
     }
 
+
 }
 
 cards.forEach(card => card.addEventListener('click', highlight));
 
 
-let allCards= document.getElementsByClassName('grid-item');
+let allCards = document.getElementsByClassName('grid-item');
+
+
+// timer
+let second = 0, minute = 0;
+let timer = document.querySelector(".timer");
+let interval;
+
+function startTimer() {
+    interval = setInterval(function () {
+        timer.innerHTML = minute + " min " + second + " s";
+        second++;
+        if (second === 60) {
+            minute++;
+            second = 0;
+        }
+        if (minute === 60) {
+            hour++;
+            minute = 0;
+        }
+    }, 1000);
+}
 
 function reset() {
-    for(let i=0;i<allCards.length;i++){
+    for (let i = 0; i < allCards.length; i++) {
         let item = allCards[i];
-        if(item.classList.contains('invisible')) {
+        if (item.classList.contains('invisible')) {
             item.classList.remove('invisible', 'hlight');
         }
     }
-
+    let timer = document.querySelector(".timer");
+    timer.innerHTML = "0 min 0 s";
+    clearInterval(interval);
 }
+
